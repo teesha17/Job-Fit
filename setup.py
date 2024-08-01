e402e8b856a02e4ebe25d5d8d5bebaacb494ba14
teesha17 <teeshu.2179@gmail.com>
2024-08-01 12:45:00 +0530

    added project

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+## edit below variables as per your requirements -
+REPO_NAME = "Job-Fit"
+AUTHOR_USER_NAME = "teesha17"
+SRC_REPO = "src"
+LIST_OF_REQUIREMENTS = ['streamlit']
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for ATS tracking",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    author_email="teeshu.2179@gmail.com",
+    packages=[SRC_REPO],
+    python_requires=">=3.7",
+    install_requires=LIST_OF_REQUIREMENTS
+)
